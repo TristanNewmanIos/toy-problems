6ace2aaeb85a4be5f5167e8b50d4086f67a1c93f
tristan <tristanb.newman@gmail.com>
2019-06-05 09:05:13 -0500

    Added the remove mininmum problem

---
+++ square-every-int-digit.js
@@ -0,0 +1,7 @@
+// Write a function to square every digit of an integer.
+
+// For example: 
+// squareEveryNum(9119) ==> 811181
+// squareEveryNum(4232) ==> 16494
+
+// Note: The function should accept an integer and return an integer
--- super-size-int/index.js
+++ super-size-int/index.js
@@ -4,3 +4,13 @@
 // superSize(105) // 510
 // superSize(12) // 21
 // If the argument passed through is single digit or is already the maximum possible integer, your function should simply return it.
+
+function superSize(int){
+    let intStr = int.toString()
+    const strArr = intStr.split('')
+    const result = parseInt(strArr.sort().reverse().join(''))
+  
+    return result
+  }
+  
+  console.log(superSize(12))
