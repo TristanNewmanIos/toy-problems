ba5fd54bd62a6f343587a053f60dd474c577bf87
tristan <tristanb.newman@gmail.com>
2019-06-08 06:54:15 -0500

    solution to the alpha-beta problem

--- alpha-beta/index.js
+++ alpha-beta/index.js
@@ -5,8 +5,27 @@
 // For a greater challenge, try to solve this problem using an array method like .forEach or .map
 
 function ABCheck(str){
+    let strArray = str.split("")
+    let aToBCount = 0
   
-}
-
-ABCheck("lane borrowed") //true
-ABCheck("play basketball") //false
+    strArray.map(char =>{
+  
+      console.log(aToBCount + " " + char)
+      
+      if((aToBCount == 3 && char === 'a') || (aToBCount == 3 && char === 'b')){
+        // console.log(true)
+        return true
+      }
+      else if(char === 'a' || char === 'b'){
+        aToBCount = 0
+      }
+      else{
+        aToBCount++
+      }
+    })
+  
+    return false
+  }
+  
+  ABCheck("lane borrowed") //true
+  ABCheck("play basketball") //false
