ccb7516b361af2b82f0a00cb1f81e894407e517c
tristan <tristanb.newman@gmail.com>
2019-06-08 06:32:33 -0500

    Added alpha-beta toy problem

---
+++ alpha-beta/index.js
@@ -0,0 +1,12 @@
+// Write a function called ABCheck that takes in a string parameter and returns true if the characters a and b are separated by exactly 3 places anywhere in the string at least once (ie. "lane borrowed" would result in true because there is exactly three characters between a and b).
+
+// Otherwise return false.
+
+// For a greater challenge, try to solve this problem using an array method like .forEach or .map
+
+function ABCheck(str){
+  
+}
+
+ABCheck("lane borrowed") //true
+ABCheck("play basketball") //false
