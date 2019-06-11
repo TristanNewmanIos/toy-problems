97187a581bc53fe3f0ff61ba6b9be91b38ac567e
tristan <tristanb.newman@gmail.com>
2019-06-11 08:17:32 -0500

    Quick and dirty solution to pete's bakery

--- pete-baker/index.js
+++ pete-baker/index.js
@@ -9,3 +9,42 @@
 // // must return 0
 // cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
 
+// Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
+
+// Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
+
+// Examples:
+
+// // must return 2
+// cakes({flour: 500, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}); 
+// // must return 0
+// cakes({apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000});
+
+function cakes(recipe, ingredients){
+    let numOfCakes = 0
+    let reqIngs = Object.keys(recipe)
+    let isZero = false
+  
+    reqIngs.map(reqIng =>{
+      let reqAmt = recipe[reqIng]
+  
+      if(ingredients[reqIng]){
+        let sets = parseInt(ingredients[reqIng] / reqAmt)
+  
+        // console.log(sets)
+  
+        if(sets === 0){
+          // console.log("should return 0")
+          isZero = true
+        }
+        else if((sets < numOfCakes || numOfCakes == 0) && !isZero){
+          numOfCakes = sets
+        }
+      }
+     
+    })
+  
+    return numOfCakes
+  }
+  
+  console.log(cakes({flour: 1300, sugar: 200, eggs: 1}, {flour: 1200, sugar: 1200, eggs: 5, milk: 200}))
